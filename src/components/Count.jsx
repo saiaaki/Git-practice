1ce1ce50f72e1dcafa0b86f81267019919ca1315
saimania@italentdigital.com <saimaniaaki402@gmail.com>
2024-03-14 14:58:55 +0530

    second commit

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -17,8 +17,9 @@ function Count() {
 
   return (
     <div>
+    <h1>React USESTATE Examples</h1>
       <h2>Counter</h2>
-      <p>Count: {count}</p>
+      <p>Counter: {count}</p>
       <button onClick={increment}>Increment</button>
       <button onClick={decrement}>Decrement</button>
       <button onClick={reset}>Reset</button>
